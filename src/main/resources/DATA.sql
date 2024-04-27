a26f93b079990da96bae13e7c08f9ac30cdacdb4
lindolfo <lindolfomoizinho1@gmail.com>
2024-04-27 18:22:07 -0300

    database: inserts to role table

---
+++ src/main/resources/DATA.sql
@@ -0,0 +1,3 @@
+INSERT IGNORE INTO tb_roles (role_id, name) VALUES (1, 'ADMIN')
+INSERT IGNORE INTO tb_roles (role_id, name) VALUES (2, 'MODERATOR')
+INSERT IGNORE INTO tb_roles (role_id, name) VALUES (3, 'USER')
