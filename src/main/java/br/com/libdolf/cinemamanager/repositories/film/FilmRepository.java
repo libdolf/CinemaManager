5b0218e786ba1e8b5cc24626ac5529789a7c2fff
lindolfo <lindolfomoizinho1@gmail.com>
2024-04-27 18:13:49 -0300

    feat: add new queries

--- src/main/java/br/com/libdolf/cinemamanager/repositories/film/FilmRepository.java
+++ src/main/java/br/com/libdolf/cinemamanager/repositories/film/FilmRepository.java
@@ -1,7 +1,21 @@
 package br.com.libdolf.cinemamanager.repositories.film;
 
 import br.com.libdolf.cinemamanager.entities.film.Film;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface FilmRepository extends JpaRepository<Film, Long> {
+    Page<Film> findFilmsByIsOnShowingTrue(Pageable pageable);
+    Optional<Film> findFilmByTitle(String title);
+    Page<Film> findFilmsByCategoriesIsContainingIgnoreCase(String categories, Pageable pageable);
+    Page<Film> findFilmsByTitleContainingIgnoreCaseAndCategoriesContainingIgnoreCase(String title, String category, Pageable pageable);
+    Page<Film> findFilmsByIsApprovedFalse(Pageable pageable);
+    List<Film> findFilmsByIsApprovedFalse();
+
+
+
 }
