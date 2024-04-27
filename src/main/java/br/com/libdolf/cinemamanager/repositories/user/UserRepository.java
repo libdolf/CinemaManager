5961d87ee136ea549a506a408dd1f0e2a31f8c54
lindolfo <lindolfomoizinho1@gmail.com>
2024-04-27 18:16:57 -0300

    feat: add role and user repositories

---
+++ src/main/java/br/com/libdolf/cinemamanager/repositories/user/UserRepository.java
@@ -0,0 +1,12 @@
+package br.com.libdolf.cinemamanager.repositories.user;
+
+import br.com.libdolf.cinemamanager.entities.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+}
